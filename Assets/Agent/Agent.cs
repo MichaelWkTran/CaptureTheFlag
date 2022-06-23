2d5d342bc84fcaf5ebdeddfef226aa5c2c489d33
MichaelWkTran <Michael.wktran@gmail.com>
2022-06-23 22:03:38 +1200

    Update

--- Assets/Agent/Agent.cs
+++ Assets/Agent/Agent.cs
@@ -18,7 +18,10 @@ public class Agent : MonoBehaviour
     public bool m_OnOpponentSide = false;
     public bool m_HaveFlag = false;
     public bool m_SavedAgent = false;
+    [SerializeField] GameObject m_AgentPrefab;
     GameController m_GameController;
+    GameController.Team m_Team;
+    GameController.Team m_OpposingTeam;
 
     [Header("[Movement Variables]")]
     [SerializeField] float m_MaxVelocity;
@@ -64,22 +67,23 @@ public class Agent : MonoBehaviour
     [Header("[Component Variables]")]
     public Rigidbody2D m_RigidBody;
 
-    public struct SteeringBehaviour
-    {
-        public delegate Vector2 SteeringDelegate();
-        public SteeringDelegate Behaviour;
-        public float Weight;
-
-        public SteeringBehaviour(SteeringDelegate _Behaviour, float _Weight)
-        {
-            Behaviour = _Behaviour;
-            Weight = _Weight;
-        }
-    }
-
     void Start()
     {
         m_GameController = FindObjectOfType<GameController>();
+
+        //Add itself to its respective team
+        if (tag == "Player")
+        {
+            m_GameController.m_PlayerTeam.Agents.Add(this);
+            m_Team = m_GameController.m_PlayerTeam;
+            m_OpposingTeam = m_GameController.m_EnemyTeam;
+        }
+        else
+        {
+            m_GameController.m_EnemyTeam.Agents.Add(this);
+            m_Team = m_GameController.m_EnemyTeam;
+            m_OpposingTeam = m_GameController.m_PlayerTeam;
+        }
     }
 
     void Update()
@@ -91,10 +95,10 @@ public class Agent : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit2D Hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
             if (Hit.collider != null)
-            {
-                if (Hit.transform.tag == "Player" && Hit.transform.gameObject == gameObject) m_GameController.m_PlayerTeam.Attacker = this;
-            }
+                if (Hit.transform.tag == "Player" && Hit.transform.gameObject == gameObject)
+                    m_GameController.m_PlayerTeam.Attacker = this;
         }
 
         //Check whether on the opponent side
@@ -113,14 +117,12 @@ public class Agent : MonoBehaviour
         //If they have a flag, decrease the opponent flag count
         if (m_OnOpponentSide == false)
         {
-            GameController.Team OpponentTeam = (tag == "Player") ? m_GameController.m_EnemyTeam : m_GameController.m_PlayerTeam;
-
             if (m_HaveFlag)
             {
-                OpponentTeam.Flags--;
+                m_OpposingTeam.Flags--;
                 m_HaveFlag = false;
             }
-            else if(m_SavedAgent)
+            else if (m_SavedAgent)
             {
                 m_SavedAgent = false;
             }
@@ -154,13 +156,115 @@ public class Agent : MonoBehaviour
         }
     }
 
+    void OnDestroy()
+    {
+        if (m_Team.Attacker = this) m_Team.Attacker = null;
+        m_Team.Agents.Remove(this);
+    }
+
+    void OnCollisionEnter2D(Collision2D _Other)
+    {
+        #region Collision With Agent
+        string OpponentTag = (tag == "Player") ? "Enemy" : "Player";
+        if (_Other.transform.tag == OpponentTag && m_OnOpponentSide)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        #endregion Collision With Agent
+    }
+
+    void OnTriggerEnter2D(Collider2D _Other)
+    {
+        #region Collision With FlagArea
+        if (_Other.gameObject == m_OpposingTeam.FlagArea && !m_SavedAgent)
+        {
+            m_HaveFlag = true;
+            if (tag == "Enemy")
+            {
+                m_Team.Attacker = null;
+            }
+        }
+
+        #endregion Collision With FlagArea
+
+        #region Collision With PrisonArea
+        if
+        (
+            _Other.gameObject == m_OpposingTeam.PrisonArea &&
+            !m_SavedAgent && !m_HaveFlag &&
+            m_Team.Agents.Count < m_GameController.m_MaxAgents
+        )
+        {
+            m_SavedAgent = true;
+            Instantiate(m_AgentPrefab).GetComponent<Agent>().m_SavedAgent = true;
+            if (tag == "Enemy")
+            {
+                m_Team.Attacker = null;
+            }
+        }
+
+        #endregion Collision With PrisonArea
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        switch (m_DrawSteeringBehaviour)
+        {
+            case DrawSteeringBehaviour.Arrive:
+                {
+                    Gizmos.DrawWireSphere(m_SteeringTarget, m_SlowingRadius);
+                    break;
+                }
+            case DrawSteeringBehaviour.PursueEvade:
+                {
+                    Gizmos.DrawLine(transform.position, transform.position + (transform.up * m_MaxPrediction));
+                    break;
+                }
+            case DrawSteeringBehaviour.Wander:
+                {
+                    Vector3 WanderTarget = transform.position + (transform.up * m_WanderDistance);
+                    Gizmos.DrawWireSphere(WanderTarget, m_WanderRadius);
+
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawWireSphere(m_SteeringTarget, 0.5f);
+                    break;
+                }
+            case DrawSteeringBehaviour.Separation:
+                {
+                    Gizmos.DrawWireSphere(transform.position, m_SeparationDistance + GetComponent<CircleCollider2D>().radius);
+                    break;
+                }
+            case DrawSteeringBehaviour.ObstacleAvoidance:
+                {
+                    for (uint i = 0; i < m_RayNumber; i++)
+                    {
+                        Gizmos.DrawLine
+                        (
+                            transform.position,
+                            transform.position +
+                            (
+                                Quaternion.Euler(0.0f, 0.0f, i * 360.0f / m_RayNumber) *
+                                transform.up *
+                                m_WallSightLength
+                            )
+                        );
+                    }
+
+
+                    break;
+                }
+        }
+    }
+
     #region States
     public void DefaultFixedUpdate()
     {
         m_ChaseMethod = Mathf.Round(Random.value) == 0;
         bool NearBorder = false;
         
-        //Check whether on the opponent side
+        //Check whether near the opponent side
         if (tag == "Player")
         {
             //The Left side is the enemy side
@@ -172,17 +276,16 @@ public class Agent : MonoBehaviour
             NearBorder = transform.position.x > -m_TeamBorderDistance;
         }
 
+        Vector2 WallAvoidance()
+        {
+            return SteeringBehaviours2D.WallAvoidance
+            (
+                transform.position, m_RigidBody.velocity, transform.up, m_MaxVelocity,
+                m_WallLayerMask, m_WallSightLength, m_RayNumber
+            );
+        }
         if (!NearBorder)
         {
-            Vector2 WallAvoidance()
-            {
-                return SteeringBehaviours2D.WallAvoidance
-                (
-                    transform.position, m_RigidBody.velocity, transform.up, m_MaxVelocity,
-                    m_WallLayerMask, m_WallSightLength, m_RayNumber
-                );
-            }
-
             Vector2 Wander()
             {
                 return SteeringBehaviours2D.Wander
@@ -227,15 +330,55 @@ public class Agent : MonoBehaviour
             Vector2 ReturnPoint = new Vector2(-100, 0);
             if (tag == "Player") ReturnPoint = new Vector2(100, 0);
 
-            m_Force = SteeringBehaviours2D.Seek(transform.position, m_RigidBody.velocity, m_MaxVelocity, ReturnPoint);
+            Vector2 Seek()
+            {
+                return SteeringBehaviours2D.Seek(transform.position, m_RigidBody.velocity, m_MaxVelocity, ReturnPoint);
+            }
+
+            Vector2 ObstacleAvoidance()
+            {
+                Collider2D[] Colliders = Physics2D.OverlapCircleAll(transform.position, GetComponent<CircleCollider2D>().radius + m_SeparationDistance);
+                List<Collider2D> Neighbours = new List<Collider2D>();
+
+                foreach (Collider2D Collider in Colliders)
+                {
+                    if
+                    (
+                        Collider.GetComponent<Agent>() == null ||
+                        Collider.tag == tag
+                    )
+                    {
+                        continue;
+                    }
+
+                    Neighbours.Add(Collider);
+                }
+
+                if (Neighbours.Count <= 0) return Vector2.zero;
+
+                return SteeringBehaviours2D.ObstacleAvoidance
+                (
+                    transform.position,
+                    transform.up, transform.right,
+                    m_RigidBody.velocity,
+                    m_MaxVelocity,
+                    m_LocalSpaceWidth,
+                    1.0f,
+                    Neighbours.ToArray()
+               );
+            }
+            
+            SteeringBehaviours2D.BehaviourDelegate[] SteeringBehaviours = new SteeringBehaviours2D.BehaviourDelegate[3]
+                { ObstacleAvoidance, Seek, WallAvoidance};
+
+            m_Force = SteeringBehaviours2D.WeightedTruncatedRunningSumWithPrioritization(SteeringBehaviours, m_MaxForce);
         }
     }
 
     public void ChaseFixedUpdate()
     {
-        Agent AttackingAgent = (tag == "Player") ? m_GameController.m_EnemyTeam.Attacker : m_GameController.m_PlayerTeam.Attacker;
-        if (AttackingAgent == null) return;
-        if (!AttackingAgent.m_OnOpponentSide) return;
+        if (m_OpposingTeam.Attacker == null) return;
+        if (!m_OpposingTeam.Attacker.m_OnOpponentSide) return;
 
         SteeringBehaviours2D.BehaviourDelegate[] SteeringBehaviours = new SteeringBehaviours2D.BehaviourDelegate[2];
 
@@ -247,8 +390,8 @@ public class Agent : MonoBehaviour
                 (
                     transform.position, m_RigidBody.velocity, m_MaxVelocity,
                     m_MaxPrediction,
-                    AttackingAgent.transform.position,
-                    AttackingAgent.m_RigidBody.velocity
+                    m_OpposingTeam.Attacker.transform.position,
+                    m_OpposingTeam.Attacker.m_RigidBody.velocity
                 );  
             }
             SteeringBehaviours[0] = Pursue;
@@ -257,7 +400,7 @@ public class Agent : MonoBehaviour
         {
             Vector2 Seek()
             {
-                return SteeringBehaviours2D.Seek(transform.position, m_RigidBody.velocity, m_MaxVelocity, AttackingAgent.transform.position);
+                return SteeringBehaviours2D.Seek(transform.position, m_RigidBody.velocity, m_MaxVelocity, m_OpposingTeam.Attacker.transform.position);
             }
             SteeringBehaviours[0] = Seek;
         }
@@ -293,7 +436,59 @@ public class Agent : MonoBehaviour
 
     public void AttackFixedUpdate()
     {
+        Vector3 m_TargetPosition = m_GameController.m_EnemyAttackerGoal ? m_OpposingTeam.FlagArea.transform.position : m_OpposingTeam.PrisonArea.transform.position;
 
+        Vector2 Seek()
+        {
+            return SteeringBehaviours2D.Seek(transform.position, m_RigidBody.velocity, m_MaxVelocity, m_TargetPosition);
+        }
+
+        Vector2 WallAvoidance()
+        {
+            return SteeringBehaviours2D.WallAvoidance
+            (
+                transform.position, m_RigidBody.velocity, transform.up, m_MaxVelocity,
+                m_WallLayerMask, m_WallSightLength, m_RayNumber
+            );
+        }
+
+        Vector2 ObstacleAvoidance()
+        {
+            Collider2D[] Colliders = Physics2D.OverlapCircleAll(transform.position, GetComponent<CircleCollider2D>().radius + m_SeparationDistance);
+            List<Collider2D> Neighbours = new List<Collider2D>();
+
+            foreach (Collider2D Collider in Colliders)
+            {
+                if
+                (
+                    Collider.GetComponent<Agent>() == null ||
+                    Collider.tag == tag
+                )
+                {
+                    continue;
+                }
+
+                Neighbours.Add(Collider);
+            }
+
+            if (Neighbours.Count <= 0) return Vector2.zero;
+
+            return SteeringBehaviours2D.ObstacleAvoidance
+            (
+                transform.position,
+                transform.up, transform.right,
+                m_RigidBody.velocity,
+                m_MaxVelocity,
+                m_LocalSpaceWidth,
+                1.0f,
+                Neighbours.ToArray()
+           );
+        }
+
+        SteeringBehaviours2D.BehaviourDelegate[] SteeringBehaviours = new SteeringBehaviours2D.BehaviourDelegate[3]
+                { ObstacleAvoidance, Seek, WallAvoidance};
+
+        m_Force = SteeringBehaviours2D.WeightedTruncatedRunningSumWithPrioritization(SteeringBehaviours, m_MaxForce);
     }
 
     #endregion States
@@ -301,72 +496,31 @@ public class Agent : MonoBehaviour
     #region Transitions
     public bool DefaultToChaseUpdate()
     {
-        GameController.Team OpponentTeam = (tag == "Player") ? m_GameController.m_EnemyTeam : m_GameController.m_PlayerTeam;
-
-        if (OpponentTeam.Attacker == null) return false;
-        if (OpponentTeam.Attacker.m_OnOpponentSide) return true;
+        if (m_OpposingTeam.Attacker == null) return false;
+        if (m_OpposingTeam.Attacker.m_OnOpponentSide) return true;
 
         return false;
     }
 
     public bool ChaseToDefaultUpdate()
     {
-        GameController.Team OpponentTeam = (tag == "Player") ? m_GameController.m_EnemyTeam : m_GameController.m_PlayerTeam;
-        if (OpponentTeam.Attacker == null) return true;
-        if (OpponentTeam.Attacker.m_OnOpponentSide) return false;
+        if (m_OpposingTeam.Attacker == null) return true;
+        if (m_OpposingTeam.Attacker.m_OnOpponentSide) return false;
 
         return true;
     }
 
-    #endregion Transitions
-
-    void OnDrawGizmosSelected()
+    public bool DefaultToAttack()
     {
-        switch (m_DrawSteeringBehaviour)
-        {
-            case DrawSteeringBehaviour.Arrive:
-            {
-                Gizmos.DrawWireSphere(m_SteeringTarget, m_SlowingRadius);
-                break;
-            }
-            case DrawSteeringBehaviour.PursueEvade:
-            {
-                Gizmos.DrawLine(transform.position, transform.position + (transform.up * m_MaxPrediction));
-                break;
-            }
-            case DrawSteeringBehaviour.Wander:
-            {
-                Vector3 WanderTarget = transform.position + (transform.up * m_WanderDistance);
-                Gizmos.DrawWireSphere(WanderTarget, m_WanderRadius);
-
-                Gizmos.color = Color.blue;
-                Gizmos.DrawWireSphere(m_SteeringTarget, 0.5f);
-                break;
-            }
-            case DrawSteeringBehaviour.Separation:
-            {
-                Gizmos.DrawWireSphere(transform.position, m_SeparationDistance + GetComponent<CircleCollider2D>().radius);
-                break;
-            }
-            case DrawSteeringBehaviour.ObstacleAvoidance:
-            {
-                for (uint i = 0; i < m_RayNumber; i++)
-                {
-                    Gizmos.DrawLine
-                    (
-                        transform.position,
-                        transform.position + 
-                        (
-                            Quaternion.Euler(0.0f, 0.0f, i * 360.0f / m_RayNumber) * 
-                            transform.up * 
-                            m_WallSightLength
-                        )
-                    );
-                }
-
-
-                break;
-            }
-        }
+        return m_Team.Attacker == this && tag == "Enemy";
     }
+
+    public bool AttackToDefault()
+    {
+        if (m_HaveFlag || m_SavedAgent) return true;
+
+        return false;
+    }
+
+    #endregion Transitions
 }
--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [System.Serializable]
-    public struct Team
+    public class Team
     {
-        public Agent[] Agents;
+        public List<Agent> Agents;
         public int Flags;
         public int AgentsCaptured;
         public Agent Attacker;
@@ -19,57 +19,67 @@ public class GameController : MonoBehaviour
 
         [Header("[UI Variables]")]
         public TMPro.TextMeshProUGUI FlagCount;
-        public TMPro.TextMeshProUGUI CapturedCount;
+        public TMPro.TextMeshProUGUI PlayerCount;
     }
 
-    [SerializeField] Cinemachine.CinemachineVirtualCamera m_Camera;
+    public Cinemachine.CinemachineVirtualCamera m_Camera;
     public Team m_PlayerTeam;
     public Team m_EnemyTeam;
 
-    [SerializeField] int m_MaxFlags = 3;
+    public int m_MaxFlags = 4;
+    public int m_MaxAgents = 4;
 
-    void Start()
+    [Header("[Enemy Variables]")]
+    public bool m_EnemyAttackerGoal = true; //True = Get flag, False = Get Agent
+    public float m_MinEnemyAttackerTime;
+    public float m_MaxEnemyAttackerTime;
+
+    void Awake()
     {
         //Setup Player Team
-        {
-            m_PlayerTeam.Flags = m_MaxFlags;
-
-            //Find Players
-            GameObject[] Players = GameObject.FindGameObjectsWithTag("Player");
-            m_PlayerTeam.Agents = new Agent[Players.Length];
-            
-            int PlayerIndex = 0;
-            foreach(GameObject Player in Players)
-            {
-                m_PlayerTeam.Agents[PlayerIndex] = Player.GetComponent<Agent>();
-                PlayerIndex++;
-            }
-
-            m_PlayerTeam.Attacker = m_PlayerTeam.Agents[0];
-        }
+        m_PlayerTeam.Flags = m_MaxFlags;
+        m_PlayerTeam.Agents = new List<Agent>();
 
         //Setup Enemy Team
-        {
-            m_EnemyTeam.Flags = m_MaxFlags;
+        m_EnemyTeam.Flags = m_MaxFlags;
+        m_EnemyTeam.Agents = new List<Agent>();
 
-            //Find Enemies
-            GameObject[] Enemies = GameObject.FindGameObjectsWithTag("Enemy");
-            m_EnemyTeam.Agents = new Agent[Enemies.Length];
-
-            int EnemyIndex = 0;
-            foreach (GameObject Enemy in Enemies)
-            {
-                m_EnemyTeam.Agents[EnemyIndex] = Enemy.GetComponent<Agent>();
-                EnemyIndex++;
-            }
-        }
+        //--------------------
+        StartCoroutine(ChooseEnemyAttacker());
     }
 
     void Update()
     {
-        m_Camera.Follow = m_PlayerTeam.Attacker.transform;
+        //Replace player team attacker if captured and set camera follow target
+        if (m_PlayerTeam.Agents.Count > 0)
+        {
+            if (m_PlayerTeam.Attacker == null) m_PlayerTeam.Attacker = m_PlayerTeam.Agents[0];
+            m_Camera.Follow = m_PlayerTeam.Attacker.transform;
+        }
 
+        //Update Player UI
         m_PlayerTeam.FlagCount.text = "Flags:" + m_PlayerTeam.Flags.ToString();
-        m_PlayerTeam.CapturedCount.text = "Captured:" + m_PlayerTeam.AgentsCaptured.ToString();
+        m_PlayerTeam.PlayerCount.text = "Players Remaining:" + m_PlayerTeam.Agents.Count.ToString();
+
+        //Update Enemy UI
+        m_EnemyTeam.FlagCount.text = "Flags:" + m_EnemyTeam.Flags.ToString();
+        m_EnemyTeam.PlayerCount.text = "Players Remaining:" + m_EnemyTeam.Agents.Count.ToString();
+    }
+
+    IEnumerator ChooseEnemyAttacker()
+    {
+        yield return new WaitForSeconds(Random.Range(m_MinEnemyAttackerTime, m_MaxEnemyAttackerTime));
+        StartCoroutine(ChooseEnemyAttacker());
+
+        if (m_EnemyTeam.Attacker == null)
+        {
+            float fGetFlagScore = Random.Range(0.0f, (m_EnemyTeam.Flags - m_MaxFlags) / m_MaxFlags);
+            float fGetAgentScore = Random.Range(0.0f, (m_EnemyTeam.Agents.Count - m_MaxAgents) / m_MaxAgents);
+
+            if (fGetFlagScore > fGetAgentScore) m_EnemyAttackerGoal = true;
+            else m_EnemyAttackerGoal = false;
+
+            m_EnemyTeam.Attacker = m_EnemyTeam.Agents[0];
+        }
     }
 }
